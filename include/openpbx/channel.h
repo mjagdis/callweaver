e0bbb28411898945e2f60db08b410428988dad2a
jcollie <jcollie@e56c2a57-502e-0410-a6e2-86b2f1ef08c6>
2005-11-17 15:47:35 +0000

    Print filename and line number in DO_CRASH.  Patch from #48.  Thanks many...
    git-svn-id: https://svn.callweaver.org/openpbx/trunk@962 e56c2a57-502e-0410-a6e2-86b2f1ef08c6

--- include/openpbx/channel.h
+++ include/openpbx/channel.h
@@ -1067,7 +1067,7 @@ static inline int opbx_select(int nfds, fd_set *rfds, fd_set *wfds, fd_set *efds
 #endif
 
 #ifdef DO_CRASH
-#define CRASH do { fprintf(stderr, "!! Forcing immediate crash a-la abort !!\n"); *((int *)0) = 0; } while(0)
+#define CRASH do { fprintf(stderr, "!! Forcing immediate crash a-la abort !!\nFile %s Line %d\n\n", __FILE__, __LINE__); *((int *)0) = 0; } while(0)
 #else
 #define CRASH do { } while(0)
 #endif
