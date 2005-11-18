eb63bbc158625bcec7578760e70188e9f7c7a386
mochouinard <mochouinard@e56c2a57-502e-0410-a6e2-86b2f1ef08c6>
2005-11-18 00:41:08 +0000

    silence compiler warnings when ZT_TONEDETECT is not defined (astbug5470)
    git-svn-id: https://svn.callweaver.org/openpbx/trunk@1028 e56c2a57-502e-0410-a6e2-86b2f1ef08c6

--- channels/chan_zap.c
+++ channels/chan_zap.c
@@ -3152,7 +3152,9 @@ static void zt_link(struct zt_pvt *slave, struct zt_pvt *master) {
 
 static void disable_dtmf_detect(struct zt_pvt *p)
 {
+#ifdef ZT_TONEDETECT
 	int val;
+#endif
 
 	p->ignoredtmf = 1;
 
@@ -3165,7 +3167,9 @@ static void disable_dtmf_detect(struct zt_pvt *p)
 
 static void enable_dtmf_detect(struct zt_pvt *p)
 {
+#ifdef ZT_TONEDETECT
 	int val;
+#endif
 
 	p->ignoredtmf = 0;
 
