15de6c006dbac001ad1d7cd2438018b11a710540
jcollie <jcollie@e56c2a57-502e-0410-a6e2-86b2f1ef08c6>
2006-03-07 04:30:48 +0000

    Apply contentlength.patch from #75.
    git-svn-id: https://svn.callweaver.org/openpbx/trunk@1506 e56c2a57-502e-0410-a6e2-86b2f1ef08c6

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -4794,8 +4794,9 @@ static int add_t38_sdp(struct sip_request *resp, struct sip_pvt *p)
 	opbx_build_string(&a_modem_next, &a_modem_left, "a=T38FaxMaxDatagram:%d\r\n",x);
 	if (p->t38jointcapability != T38FAX_UDP_EC_NONE)
 		opbx_build_string(&a_modem_next, &a_modem_left, "a=T38FaxUdpEC:%s\r\n", (p->t38jointcapability & T38FAX_UDP_EC_REDUNDANCY) ? "t38UDPRedundancy" : "t38UDPFEC");
-	if (p->udptl)
-		len = strlen(m_modem) + strlen(a_modem);
+
+	len = strlen(v) + strlen(o) + strlen(s) + strlen(c) + strlen(t) + strlen(m_modem) + strlen(a_modem);
+
 	add_header(resp, "Content-Type", "application/sdp");
 	add_header_contentLength(resp, len);
 	add_line(resp, v);
