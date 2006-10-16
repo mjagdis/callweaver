707241ba072ca1ba05fa759c234d1f6842dfbd84
ctrix <ctrix@e56c2a57-502e-0410-a6e2-86b2f1ef08c6>
2006-10-16 00:21:05 +0000

    Improve TALK detection is faxdetect. Add new options and channel variables to support OGI.
    git-svn-id: https://svn.callweaver.org/openpbx/trunk@1974 e56c2a57-502e-0410-a6e2-86b2f1ef08c6

--- apps/app_faxdetect.c
+++ apps/app_faxdetect.c
@@ -56,31 +56,45 @@ static char *synopsis = "Detects fax sounds on all channel types (IAX and SIP to
 
 static char *descrip = 
 "  FaxDetect([waitdur[|tonestr[|options[|sildur[|mindur[|maxdur]]]]]]):\n"
-"This application listens for fax tones (on IAX and SIP channels too)\n"
-"for waitdur seconds of time(default is 4 seconds. In addition, \n"
-"it can play optional ringtone indicated by tonestr \n"
-"and it can be interrupted by digits or non-silence. \n"
-"Audio is only monitored in the receive direction. If\n"
-"digits interrupt, they must be the start of a valid extension unless the\n"
-"option is included to ignore. If fax is detected, it will jump to the\n"
-"'fax' extension. If a period of non-silence greater than 'mindur' ms,\n"
-"yet less than 'maxdur' ms is followed by silence at least 'sildur' ms\n"
-"then the app is aborted and processing jumps to the 'talk' extension.\n"
-"if you have entered some DTMF digits, the TALK_DTMF_DID channel variable\n"
-"will be set. This allows interaction with OGI scripts.\n"
-"If all undetected, control will continue at the next priority.\n"
+"Parameters:\n"
 "      waitdur:  Maximum number of seconds to wait (default=4)\n"
+"      tonestr:  Indication to be used while detecting (example: ring)\n"
 "      options:\n"
-"        'n':  Attempt on-hook if unanswered (default=no)\n"
-"        'x':  DTMF digits terminate without extension (default=no)\n"
-"        'd':  Ignore DTMF digit detection (default=no)\n"
-"        'f':  Ignore fax detection (default=no)\n"
-"        't':  Ignore talk detection (default=no)\n"
-"      sildur:  Silence ms after mindur/maxdur before aborting (default=1000)\n"
-"      mindur:  Minimum non-silence ms needed (default=100)\n"
-"      maxdur:  Maximum non-silence ms allowed (default=0/forever)\n"
-"Returns -1 on hangup, and 0 on successful completion with no exit conditions.\n\n"
-"For questions or comments, please e-mail support@newmantelecom.com.\n";
+"        'n':     Attempt on-hook if unanswered (default=no)\n"
+"        'x':     DTMF digits terminate without extension (default=no)\n"
+"        'd':     Ignore DTMF digit detection (default=no)\n"
+"        'D':     DTMF digit detection and (default=no)\n"
+"                 jump do the dialled extension after pressing #\n"
+"                 Disables single digit jumps#\n"
+"        'f':     Ignore fax detection (default=no)\n"
+"        't':     Ignore talk detection (default=no)\n"
+"        'j':     To be used in OGI scripts. Does not jump to any extension.\n"
+"        sildur:  Silence ms after mindur/maxdur before aborting (default=1000)\n"
+"        mindur:  Minimum non-silence ms needed (default=100)\n"
+"        maxdur:  Maximum non-silence ms allowed (default=0/forever)\n"
+"\n"
+"This application listens for fax tones for waitdur seconds of time.\n"
+"Audio is only monitored in the receive direction.\n"
+"It can play optional ringtone indicated by tonestr. \n"
+"It can be interrupted by digits or non-silence (talk or fax tones).\n"
+"if d option is specified, a single digit interrupt and must be the \n"
+"start of a valid extension.\n"
+"If D option is specified (overrides d), the application will wait for\n"
+"the user to enter digits terminated by a # and jump to the corresponding\n"
+"extension, if it exists.\n"
+"If fax is detected (tones or T38 invite), it will jump to the 'fax' extension.\n"
+"If a period of non-silence greater than 'mindur' ms, yet less than 'maxdur' ms\n"
+"is followed by silence at least 'sildur' ms then the application jumps\n"
+"to the 'talk' extension.\n"
+"If all undetected, control will continue at the next priority.\n"
+"The application, upon exit, will set the folloging channel variables: \n"
+"   - DTMF_DETECTED : set to 1 when a DTMF digit would have caused a jump.\n"
+"   - TALK_DETECTED : set to 1 when talk has been detected.\n"
+"   - FAX_DETECTED  : set to 1 when fax tones detected.\n"
+"   - FAXEXTEN      : original dialplan extension of this application\n"
+"   - DTMF_DID      : digits dialled beforeexit (#excluded)\n"
+"Returns -1 on hangup, and 0 on successful completion with no exit conditions.\n"
+"\n";
 
 // Use the second one for recent Asterisk releases
 #define CALLERID_FIELD cid.cid_num
@@ -103,20 +117,25 @@ static int detectfax_exec(struct opbx_channel *chan, void *data)
 	char *silstr = NULL;
 	char *minstr = NULL;
 	char *maxstr = NULL;
+	int totalsilence;			// working vars
+	int ms_silence=0,ms_talk=0;		// working vars
 	struct opbx_frame *fr = NULL;
 	struct opbx_frame *fr2 = NULL;
 	struct opbx_frame *fr3 = NULL;
-	int notsilent = 0;
-	struct timeval start = {0, 0}, end = {0, 0};
+	int speaking = 0;
+	int talkdetection_started = 0;
+	struct timeval start_talk = {0, 0}, start_silence = {0, 0}, now = {0, 0};
 	int waitdur = 4;
 	int sildur = 1000;
-	int mindur = 100;
+	int mindur = 150;
 	int maxdur = -1;
 	int skipanswer = 0;
 	int noextneeded = 0;
 	int ignoredtmf = 0;
 	int ignorefax = 0;
 	int ignoretalk = 0;
+	int ignorejump = 0;
+	int longdtmf = 0;
 	int x = 0;
 	int origrformat = 0;
 	int origwformat = 0;
@@ -124,11 +143,11 @@ static int detectfax_exec(struct opbx_channel *chan, void *data)
 	time_t timeout = 0;
 	struct opbx_dsp *dsp = NULL;
 	
-	pbx_builtin_setvar_helper(chan, "FAX_DETECTED", "");
-	pbx_builtin_setvar_helper(chan, "FAXEXTEN", "");
-	pbx_builtin_setvar_helper(chan, "DTMF_DETECTED", "");
-	pbx_builtin_setvar_helper(chan, "TALK_DETECTED", "");
-        pbx_builtin_setvar_helper(chan, "TALK_DTMF_DID", "");
+	pbx_builtin_setvar_helper(chan, "FAX_DETECTED", "0");
+	pbx_builtin_setvar_helper(chan, "FAXEXTEN", "unknown");
+	pbx_builtin_setvar_helper(chan, "DTMF_DETECTED", "0");
+	pbx_builtin_setvar_helper(chan, "TALK_DETECTED", "0");
+        pbx_builtin_setvar_helper(chan, "DTMF_DID", "");
 
 	if (data || !opbx_strlen_zero((char *)data)) {
 		strncpy(tmp, (char *)data, sizeof(tmp)-1);
@@ -147,6 +166,9 @@ static int detectfax_exec(struct opbx_channel *chan, void *data)
 		if ((sscanf(waitstr, "%d", &x) == 1) && (x > 0))
 			waitdur = x;
 	}
+
+	if (!strlen(tonestr)) 
+	    tonestr=NULL;
 	
 	if (options) {
 		if (strchr(options, 'n'))
@@ -155,10 +177,16 @@ static int detectfax_exec(struct opbx_channel *chan, void *data)
 			noextneeded = 1;
 		if (strchr(options, 'd'))
 			ignoredtmf = 1;
+		if (strchr(options, 'D')) {
+			longdtmf = 1;
+			ignoredtmf = 0;
+		}
 		if (strchr(options, 'f'))
 			ignorefax = 1;
 		if (strchr(options, 't'))
 			ignoretalk = 1;
+		if (strchr(options, 'j'))
+			ignorejump = 1;
 	}
 	
 	if (silstr) {
@@ -202,8 +230,8 @@ static int detectfax_exec(struct opbx_channel *chan, void *data)
 			; /* features |= DSP_FEATURE_SILENCE_SUPPRESS; */
 		if (!ignorefax)
 			features |= DSP_FEATURE_FAX_DETECT;
-		//if (!ignoredtmf)
-			features |= DSP_FEATURE_DTMF_DETECT;
+
+		features |= DSP_FEATURE_DTMF_DETECT;
 			
 		opbx_dsp_set_threshold(dsp, 256); 
 		opbx_dsp_set_features(dsp, features | DSP_DIGITMODE_RELAXDTMF);
@@ -220,6 +248,8 @@ static int detectfax_exec(struct opbx_channel *chan, void *data)
 	    if (res)
 		opbx_log(LOG_NOTICE,"Unable to start playtones\n");
 	}
+
+
 
 	if (!res) {
 		if (waitdur > 0)
@@ -241,21 +271,23 @@ static int detectfax_exec(struct opbx_channel *chan, void *data)
 			/* Check for a T38 switchover */
 			if (chan->t38mode_enabled==1 && !ignorefax) {
 			    opbx_log(LOG_DEBUG, "Fax detected on %s. T38 switchover completed.\n", chan->name);
-			    if (strcmp(chan->exten, "fax")) {
-				opbx_log(LOG_NOTICE, "Redirecting %s to fax extension\n", chan->name);
-				pbx_builtin_setvar_helper(chan, "FAX_DETECTED", "1");
-				pbx_builtin_setvar_helper(chan,"FAXEXTEN",chan->exten);								
-				if (opbx_exists_extension(chan, chan->context, "fax", 1, chan->CALLERID_FIELD)) {
-				    /* Save the DID/DNIS when we transfer the fax call to a "fax" extension */
-				    strncpy(chan->exten, "fax", sizeof(chan->exten)-1);
-				    chan->priority = 0;									
+			    pbx_builtin_setvar_helper(chan, "FAX_DETECTED", "1");
+			    pbx_builtin_setvar_helper(chan,"FAXEXTEN",chan->exten);
+			    if (!ignorejump) {
+				if (strcmp(chan->exten, "fax")) {
+				    opbx_log(LOG_NOTICE, "Redirecting %s to fax extension [T38]\n", chan->name);
+				    if (opbx_exists_extension(chan, chan->context, "fax", 1, chan->CALLERID_FIELD)) {
+					/* Save the DID/DNIS when we transfer the fax call to a "fax" extension */
+					strncpy(chan->exten, "fax", sizeof(chan->exten)-1);
+					chan->priority = 0;									
+				    } else
+					opbx_log(LOG_WARNING, "Fax detected, but no fax extension\n");
 				} else
-				    opbx_log(LOG_WARNING, "Fax detected, but no fax extension\n");
-				} else
-				    opbx_log(LOG_WARNING, "Already in a fax extension, not redirecting\n");
-				res = 0;
-				opbx_frfree(fr);
-				break;
+			    	    opbx_log(LOG_WARNING, "Already in a fax extension, not redirecting\n");
+			    }
+			    res = 0;
+			    opbx_frfree(fr);
+			    break;
 			}
 
 
@@ -269,29 +301,33 @@ static int detectfax_exec(struct opbx_channel *chan, void *data)
 				if (fr2->subclass == 'f' && !ignorefax) {
 					/* Fax tone -- Handle and return NULL */
 					opbx_log(LOG_DEBUG, "Fax detected on %s\n", chan->name);
-					if (strcmp(chan->exten, "fax")) {
-						opbx_log(LOG_NOTICE, "Redirecting %s to fax extension\n", chan->name);
-						pbx_builtin_setvar_helper(chan, "FAX_DETECTED", "1");
-						pbx_builtin_setvar_helper(chan,"FAXEXTEN",chan->exten);								
-						if (opbx_exists_extension(chan, chan->context, "fax", 1, chan->CALLERID_FIELD)) {
-							/* Save the DID/DNIS when we transfer the fax call to a "fax" extension */
-							strncpy(chan->exten, "fax", sizeof(chan->exten)-1);
-							chan->priority = 0;									
-						} else
-							opbx_log(LOG_WARNING, "Fax detected, but no fax extension\n");
-					} else
-						opbx_log(LOG_WARNING, "Already in a fax extension, not redirecting\n");
-
+					pbx_builtin_setvar_helper(chan, "FAX_DETECTED", "1");
+					pbx_builtin_setvar_helper(chan,"FAXEXTEN",chan->exten);
+					if (!ignorejump) {
+					    if (strcmp(chan->exten, "fax")) {
+						opbx_log(LOG_NOTICE, "Redirecting %s to fax extension [DTMF]\n", chan->name);
+						    if (opbx_exists_extension(chan, chan->context, "fax", 1, chan->CALLERID_FIELD)) {
+							    /* Save the DID/DNIS when we transfer the fax call to a "fax" extension */
+							    strncpy(chan->exten, "fax", sizeof(chan->exten)-1);
+							    chan->priority = 0;									
+						    } else
+							    opbx_log(LOG_WARNING, "Fax detected, but no fax extension\n");
+					    } else
+						    opbx_log(LOG_WARNING, "Already in a fax extension, not redirecting\n");
+					}
 					res = 0;
 					opbx_frfree(fr);
 					break;
 				} else if (!ignoredtmf) {
-					opbx_log(LOG_DEBUG, "DTMF detected on %s\n", chan->name);
 					char t[2];
 					t[0] = fr2->subclass;
 					t[1] = '\0';
-					if (noextneeded || opbx_canmatch_extension(chan, chan->context, t, 1, chan->CALLERID_FIELD)) {
-						pbx_builtin_setvar_helper(chan, "DTMF_DETECTED", "1");
+					opbx_log(LOG_DEBUG, "DTMF detected on %s: %s\n", chan->name,t);
+					pbx_builtin_setvar_helper(chan, "DTMF_DETECTED", "1");
+					if (
+						( noextneeded || opbx_canmatch_extension(chan, chan->context, t, 1, chan->CALLERID_FIELD) )
+						&& !longdtmf
+					   ) {
 						/* They entered a valid extension, or might be anyhow */
 						if (noextneeded) {
 							opbx_log(LOG_NOTICE, "DTMF received (not matching to exten)\n");
@@ -303,15 +339,25 @@ static int detectfax_exec(struct opbx_channel *chan, void *data)
 						opbx_frfree(fr);
 						break;
 					} else {
-						strncat(dtmf_did,t,sizeof(dtmf_did)-strlen(dtmf_did)-1 );
+						if (strcmp(t,"#") || !longdtmf) {
+						    strncat(dtmf_did,t,sizeof(dtmf_did)-strlen(dtmf_did)-1 );
+						} else {
+						    pbx_builtin_setvar_helper(chan, "DTMF_DID", dtmf_did);
+						    if (!ignorejump && opbx_canmatch_extension(chan, chan->context, dtmf_did, 1, chan->CALLERID_FIELD) ) {
+							strncpy(chan->exten, dtmf_did, sizeof(chan->exten)-1);
+							chan->priority = 0;
+						    }
+						    res=0;
+						    opbx_frfree(fr);
+						    break;
+						}
 						opbx_log(LOG_DEBUG, "Valid extension requested and DTMF did not match [%s]\n",t);
 					}
 				}
 			} else if ((fr->frametype == OPBX_FRAME_VOICE) && (fr->subclass == OPBX_FORMAT_SLINEAR) && !ignoretalk) {
-				int totalsilence;
-				int ms;
 
-				if (!tonestr) {
+				// Let's do echo
+    				if (!tonestr) {
     				    //The following piece of code enables this application
 				    //to send empty frames. This solves fax detection problem
 				    //When a fax gets in with RTP. 
@@ -323,47 +369,55 @@ static int detectfax_exec(struct opbx_channel *chan, void *data)
 				    opbx_frfree(fr3);
 				}
 
+
 				res = opbx_dsp_silence(dsp, fr, &totalsilence);
-				if (res && (totalsilence > sildur)) {
-					/* We've been quiet a little while */
-					if (notsilent) {
-						/* We had heard some talking */
-						gettimeofday(&end, NULL);
-						ms = (end.tv_sec - start.tv_sec) * 1000;
-						ms += (end.tv_usec - start.tv_usec) / 1000;
-						ms -= sildur;
-						if (ms < 0)
-							ms = 0;
-						if ((ms > mindur) && ((maxdur < 0) || (ms < maxdur))) {
-							char ms_str[10];
-							opbx_log(LOG_DEBUG, "Found qualified token of %d ms\n", ms);
-							opbx_log(LOG_NOTICE, "Redirecting %s to talk extension\n", chan->name);
 
-							/* Save detected talk time (in milliseconds) */ 
-							sprintf(ms_str, "%d", ms);	
-							pbx_builtin_setvar_helper(chan, "TALK_DETECTED", ms_str);
-							pbx_builtin_setvar_helper(chan, "TALK_DTMF_DID", dtmf_did);
+				if (res) { // There is silence on the line.
+				    gettimeofday(&now, NULL);
+				    if (talkdetection_started && speaking) { // 1st iteration
+					ms_talk =  (now.tv_sec  - start_talk.tv_sec ) * 1000;
+					ms_talk += (now.tv_usec - start_talk.tv_usec) / 1000;
 
-							if (opbx_exists_extension(chan, chan->context, "talk", 1, chan->CALLERID_FIELD)) {
-								strncpy(chan->exten, "talk", sizeof(chan->exten) - 1);
-								chan->priority = 0;
-							} else
-								opbx_log(LOG_WARNING, "Talk detected, but no talk extension\n");
-							res = 0;
-							opbx_frfree(fr);
-							break;
+				    }
+				    if (speaking)
+					gettimeofday(&start_silence, NULL);
+
+				    ms_silence =  (now.tv_sec  - start_silence.tv_sec ) * 1000;
+				    ms_silence += (now.tv_usec - start_silence.tv_usec) / 1000;
+
+				    //opbx_log(LOG_WARNING,"[%5d,%5d,%5d] MS_TALK: %6d MS_SILENCE %6d\n", 
+					mindur,maxdur,sildur, ms_talk, ms_silence);
+				    speaking=0;
+
+				    if (ms_silence>=sildur) {
+					if ( (ms_talk>=mindur) && ( (maxdur<0) || (ms_talk<maxdur) ) ) {
+					    // TALK Has been detected
+					    char ms_str[64];
+					    snprintf(ms_str, sizeof(ms_str), "%d", ms_talk);
+					    pbx_builtin_setvar_helper(chan, "TALK_DETECTED", ms_str);
+					    if (!ignorejump) {
+					        opbx_log(LOG_NOTICE, "Redirecting %s to talk extension\n", chan->name);
+						if (opbx_exists_extension(chan, chan->context, "talk", 1, chan->CALLERID_FIELD)) {
+						    strncpy(chan->exten, "talk", sizeof(chan->exten) - 1);
+						    chan->priority = 0;
 						} else
-							opbx_log(LOG_DEBUG, "Found unqualified token of %d ms\n", ms);
-						notsilent = 0;
+						    opbx_log(LOG_WARNING, "Talk detected, but no talk extension\n");
+					    }
+					    res = 0;
+					    opbx_frfree(fr);
+					    break;
 					}
+				    }
+
 				} else {
-					if (!notsilent) {
-						/* Heard some audio, mark the begining of the token */
-						gettimeofday(&start, NULL);
-						opbx_log(LOG_DEBUG, "Start of voice token!\n");
-						notsilent = 1;
-					}
-				}						
+				    opbx_log(LOG_DEBUG,"Start of voice token\n");
+				    if (!talkdetection_started)
+					talkdetection_started=1;
+				    if (!speaking)
+					gettimeofday(&start_talk, NULL);
+				    speaking=1;
+				}
+
 			}
 			opbx_frfree(fr);
 		}
