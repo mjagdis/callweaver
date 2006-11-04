6374b9c989f08bd81b7da2ed5d546db790327493
coppice <coppice@e56c2a57-502e-0410-a6e2-86b2f1ef08c6>
2006-11-04 18:02:14 +0000

    These files lacked version tags and header. They have also been tidied a bit
    git-svn-id: https://svn.callweaver.org/openpbx/trunk@2008 e56c2a57-502e-0410-a6e2-86b2f1ef08c6

--- include/openpbx/stun.h
+++ include/openpbx/stun.h
@@ -1,35 +1,52 @@
+/*
+ * OpenPBX -- An open source telephony toolkit.
+ *
+ * See http://www.openpbx.org for more information about
+ * the OpenPBX project. Please do not directly contact
+ * any of the maintainers of this project for assistance;
+ * the project provides a web site, mailing lists and IRC
+ * channels for your use.
+ *
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License Version 2. See the LICENSE file
+ * at the top of the source tree.
+ */
 
-#define STUN_IGNORE		(0)
-#define STUN_ACCEPT		(1)
+#define STUN_IGNORE		        (0)
+#define STUN_ACCEPT		        (1)
 
-#define STUN_BINDREQ	0x0001
-#define STUN_BINDRESP	0x0101
-#define STUN_BINDERR	0x0111
-#define STUN_SECREQ	0x0002
-#define STUN_SECRESP	0x0102
-#define STUN_SECERR	0x0112
+#define STUN_BINDREQ            0x0001
+#define STUN_BINDRESP           0x0101
+#define STUN_BINDERR            0x0111
+#define STUN_SECREQ             0x0002
+#define STUN_SECRESP            0x0102
+#define STUN_SECERR             0x0112
 
-#define STUN_MAPPED_ADDRESS	0x0001
-#define STUN_RESPONSE_ADDRESS	0x0002
-#define STUN_CHANGE_REQUEST	0x0003
-#define STUN_SOURCE_ADDRESS	0x0004
-#define STUN_CHANGED_ADDRESS	0x0005
-#define STUN_USERNAME		0x0006
-#define STUN_PASSWORD		0x0007
-#define STUN_MESSAGE_INTEGRITY	0x0008
-#define STUN_ERROR_CODE		0x0009
-#define STUN_UNKNOWN_ATTRIBUTES	0x000a
-#define STUN_REFLECTED_FROM	0x000b
+#define STUN_MAPPED_ADDRESS     0x0001
+#define STUN_RESPONSE_ADDRESS   0x0002
+#define STUN_CHANGE_REQUEST     0x0003
+#define STUN_SOURCE_ADDRESS     0x0004
+#define STUN_CHANGED_ADDRESS    0x0005
+#define STUN_USERNAME           0x0006
+#define STUN_PASSWORD           0x0007
+#define STUN_MESSAGE_INTEGRITY  0x0008
+#define STUN_ERROR_CODE         0x0009
+#define STUN_UNKNOWN_ATTRIBUTES 0x000a
+#define STUN_REFLECTED_FROM     0x000b
 
-extern char               stunserver_host[MAXHOSTNAMELEN];
+extern char stunserver_host[MAXHOSTNAMELEN];
 extern struct sockaddr_in stunserver_ip;
-extern int                stunserver_portno;
-extern int 		  stun_active;			/*!< Is STUN globally enabled ?*/
-extern int 		  stundebug;			/*!< Are we debugging stun? */
+extern int stunserver_portno;
+extern int stun_active;			/*!< Is STUN globally enabled ?*/
+extern int stundebug;			/*!< Are we debugging stun? */
 
-typedef struct { unsigned int id[4]; } __attribute__((packed)) stun_trans_id;
+typedef struct
+{
+    unsigned int id[4];
+} __attribute__((packed)) stun_trans_id;
 
-struct stun_header {
+struct stun_header
+{
 	unsigned short msgtype;
 	unsigned short msglen;
 	stun_trans_id  id;
@@ -37,22 +54,23 @@ struct stun_header {
 } __attribute__((packed));
 
 
-struct stun_attr {
+struct stun_attr
+{
 	unsigned short attr;
 	unsigned short len;
 	unsigned char value[0];
 } __attribute__((packed));
 
-struct stun_addr {
+struct stun_addr
+{
 	unsigned char unused;
 	unsigned char family;
 	unsigned short port;
 	unsigned int addr;
 } __attribute__((packed));
 
-
-
-struct stun_request {
+struct stun_request
+{
 	struct stun_header    	req_head;
 	struct stun_request 	*next;
 	time_t 		      	whendone;
@@ -60,7 +78,8 @@ struct stun_request {
 	struct stun_addr	mapped_addr;
 };
 
-struct stun_state {
+struct stun_state
+{
 	unsigned short msgtype;
 	stun_trans_id  id;
 	unsigned char *username;
