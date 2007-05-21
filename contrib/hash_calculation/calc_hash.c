6184fc2bb0d37f9cec0075a604a78a1328e8dd29
mikael <mikael@e56c2a57-502e-0410-a6e2-86b2f1ef08c6>
2007-05-21 08:44:09 +0000

    A contrib app to generate hash values for use in CW
    git-svn-id: https://svn.callweaver.org/callweaver/trunk@2792 e56c2a57-502e-0410-a6e2-86b2f1ef08c6

---
+++ contrib/hash_calculation/calc_hash.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <strings.h>
+#define MAX_STR_LEN 80
+int main (int argc, const char * argv[]) {
+ unsigned int i, len, hash = 0;
+ char ch, str[MAX_STR_LEN + 1];
+ 
+ if (argc <= 1) {
+  printf("Usage: calc_hash string\nExample: ./calc_hash ACCOUNTCODE will return 0x 47D129A\n");
+  return 1;
+ }
+ else {
+  len = strlen(argv[1]);
+  if (len > MAX_STR_LEN) {
+   len = MAX_STR_LEN;
+  } // end if
+  strncpy(str, argv[1], len);
+  
+  for (i = 0; i < len; i++) {
+   ch = str[i];
+   hash = ch + (hash << 6) + (hash << 16) - hash;
+  } // end for
+  hash = (hash & 0x7FFFFFFF);
+  
+  printf("0x%8X\n", hash);
+  
+  return 0;
+ } // end if
+} // end main
+// END OF FILE
