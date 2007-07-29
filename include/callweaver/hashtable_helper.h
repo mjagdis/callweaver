5ce6c66423e76b9a93bb5d88d99911ac2cd21baf
ctrix <ctrix@e56c2a57-502e-0410-a6e2-86b2f1ef08c6>
2007-07-29 11:49:37 +0000

    Adding hashtable helper files.
    git-svn-id: https://svn.callweaver.org/callweaver/trunk@3352 e56c2a57-502e-0410-a6e2-86b2f1ef08c6

---
+++ include/callweaver/hashtable_helper.h
@@ -0,0 +1,36 @@
+/*
+ * CallWeaver -- An open source telephony toolkit.
+ *
+ * Copyright (C) 2007 - Navynet SRL
+ *
+ * Massimo Cetra <devel@navynet.it>
+ *
+ * See http://www.callweaver.org for more information about
+ * the CallWeaver project. Please do not directly contact
+ * any of the maintainers of this project for assistance;
+ * the project provides a web site, mailing lists and IRC
+ * channels for your use.
+ *
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License Version 2. See the LICENSE file
+ * at the top of the source tree.
+ */
+
+/*
+ * Hash table engine helper.
+ */
+
+#ifndef _CALLWEAVER_HASHTABLE_HELPER_H
+#define _CALLWEAVER_HASHTABLE_HELPER_H
+
+#include "callweaver/hashtable.h"
+
+
+int opbx_core_hash_insert ( hash_table_t *hash, char *key, void *data );
+int opbx_core_hash_get ( hash_table_t *hash, char *key, void **data );
+int opbx_core_hash_delete (hash_table_t *hash, char *key, int mustfree );
+
+
+
+#endif // _CALLWEAVER_HASHTABLE_HELPER_H
+
