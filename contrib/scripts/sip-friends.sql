6fa3fadbf7eac519caa05b47d0d509b4fdea7069
RoyK <RoyK@e56c2a57-502e-0410-a6e2-86b2f1ef08c6>
2007-08-27 15:32:32 +0000

    a working sipfriends table written in generic sql so it should be pretty much portable between DBMSes, and a little patch
    git-svn-id: https://svn.callweaver.org/callweaver/trunk@3827 e56c2a57-502e-0410-a6e2-86b2f1ef08c6

--- contrib/scripts/sip-friends.sql
+++ contrib/scripts/sip-friends.sql
@@ -1,14 +1,35 @@
-#
-# Table structure for table `sipfriends`
-#
+--
+-- Table structure for table `sipfriends`
+--
 
-CREATE TABLE `sipfriends` (
-  `name` varchar(40) NOT NULL default '',
-  `secret` varchar(40) NOT NULL default '',
-  `context` varchar(40) NOT NULL default '',
-  `username` varchar(40) default '',
-  `ipaddr` varchar(20) NOT NULL default '',
-  `port` int(6) NOT NULL default '0',
-  `regseconds` int(11) NOT NULL default '0',
-  PRIMARY KEY  (`name`)
-) TYPE=MyISAM;
+CREATE TABLE sipfriends (
+  name varchar(40) primary key,
+  username varchar(40),
+  secret varchar(40) NOT NULL,
+  context varchar(40) NOT NULL default 'default',
+  ipaddr varchar(40) default NULL,
+  port int(6) default '5060',
+  regseconds int(11) default NULL,
+  dtmfmode varchar(50) NOT NULL default 'inband',
+  restrictcid varchar(50) default NULL,
+  qualify varchar(10) default 'no',
+  insecure varchar(50) default NULL,
+  callerid varchar(10) default NULL,
+  disallow varchar(50) default NULL,
+  allow varchar(50) default NULL,
+  nat varchar(10) default 'yes',
+  host varchar(20) default 'dynamic',
+  fullcontact varchar(80) default NULL,
+  accountcode varchar(50) default NULL,
+  useragent varchar(255) default NULL,
+  transfer varchar(10) default 'no',
+  canreinvite varchar(10) NOT NULL default 'no',
+  pickupgroup varchar(64) NOT NULL,
+  callgroup varchar(64) NOT NULL,
+  regserver varchar(64) default NULL,
+  call_limit int(11) default '1'
+); 
+
+create unique index sipfriends_name_idx on sipfriends(name);
+create unique index sipfriends_username_idx on sipfriends(username);
+
