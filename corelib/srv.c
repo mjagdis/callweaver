6c6bec1ba11527e0e8ceedd883583ed6015db1cf
mjagdis <mjagdis@e56c2a57-502e-0410-a6e2-86b2f1ef08c6>
2008-01-31 22:21:27 +0000

    sizeof() gives unsigned int not unsigned long
    git-svn-id: https://svn.callweaver.org/callweaver/trunk@4526 e56c2a57-502e-0410-a6e2-86b2f1ef08c6

--- corelib/srv.c
+++ corelib/srv.c
@@ -69,7 +69,7 @@ static int parse_srv(char *host, int hostlen, int *portno, char *answer, int len
 	char repl[256] = "";
 
 	if (len < sizeof(struct srv)) {
-		cw_log(CW_LOG_WARNING, "Supplied buffer length too short (%d < %lu)\n", len, sizeof(struct srv));
+		cw_log(CW_LOG_WARNING, "Supplied buffer length too short (%d < %u)\n", len, sizeof(struct srv));
 		return -1;
 	}
 	answer += sizeof(struct srv);
