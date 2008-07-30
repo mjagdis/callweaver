0c014e075fd223fe8ae027e227763c73f1a6ef2a
mjagdis <mjagdis@e56c2a57-502e-0410-a6e2-86b2f1ef08c6>
2008-07-30 16:58:34 +0000

    Standardize the out of memory error message, log it as an error and close the socket we just accepted rather than leaking it.
    git-svn-id: https://svn.callweaver.org/callweaver/trunk@5079 e56c2a57-502e-0410-a6e2-86b2f1ef08c6

--- corelib/manager.c
+++ corelib/manager.c
@@ -1568,7 +1568,8 @@ static void *accept_thread(void *ignore)
 		if (setsockopt(as, SOL_TCP, TCP_NODELAY, &arg, sizeof(arg)) < 0)
 			cw_log(CW_LOG_WARNING, "Failed to set manager tcp connection to TCP_NODELAY mode: %s\n", strerror(errno));
 		if ((s = calloc(1, sizeof(struct mansession))) == NULL) {
-			cw_log(CW_LOG_WARNING, "Failed to allocate management session: %s\n", strerror(errno));
+			cw_log(CW_LOG_ERROR, "Out of memory\n");
+			close(as);
 			continue;
 		} 
 
