8a2106c9384eafbcd5d8a616eeecc30ee6790877
mjagdis <mjagdis@e56c2a57-502e-0410-a6e2-86b2f1ef08c6>
2008-12-31 15:44:09 +0000

    Change Milliwatt to use the core's tone generator Change the freq to be 1004Hz (it's offset to avoid harmonics on line digitized at an 8kHz sample rate) Mark Milliwatt deprecated and recommend using Playtones or Playback (to avoid transcoding) directly
    git-svn-id: https://svn.callweaver.org/callweaver/trunk@5287 e56c2a57-502e-0410-a6e2-86b2f1ef08c6

--- apps/app_milliwatt.c
+++ apps/app_milliwatt.c
@@ -47,86 +47,32 @@ static const char tdesc[] = "Digital Milliwatt (mu-law) Test Application";
 
 static void *milliwatt_app;
 static const char milliwatt_name[] = "Milliwatt";
-static const char milliwatt_synopsis[] = "Generate a Constant 1000Hz tone at 0dbm (mu-law)";
+static const char milliwatt_synopsis[] = "Generate a Constant 1004Hz tone at 0dbm (mu-law)";
 static const char milliwatt_syntax[] = "Milliwatt()";
 static const char milliwatt_descrip[] = 
-"Generate a Constant 1000Hz tone at 0dbm (mu-law)\n";
+"Generate a Constant 1004Hz tone at 0dbm (mu-law)\n";
 
-
-static char digital_milliwatt[] = {0x1e,0x0b,0x0b,0x1e,0x9e,0x8b,0x8b,0x9e} ;
-
-
-struct gen_state {
-	int index;
-	struct cw_frame f;
-	uint8_t buf[640 + CW_FRIENDLY_OFFSET];
-};
-
-
-static void *milliwatt_alloc(struct cw_channel *chan, void *params)
-{
-	struct gen_state *state;
-
-	if ((state = malloc(sizeof(*state))))
-		state->index = 0;
-	return state;
-}
-
-static void milliwatt_release(struct cw_channel *chan, void *data)
-{
-	free(data);
-	return;
-}
-
-static struct cw_frame *milliwatt_generate(struct cw_channel *chan, void *data, int samples)
-{
-	struct gen_state *state = data;
-	int i;
-
-	if (samples > sizeof(state->buf) - CW_FRIENDLY_OFFSET)
-		samples = sizeof(state->buf) - CW_FRIENDLY_OFFSET;
-
-	cw_fr_init_ex(&state->f, CW_FRAME_VOICE, CW_FORMAT_ULAW);
-	state->f.offset = CW_FRIENDLY_OFFSET;
-	state->f.data = state->buf + CW_FRIENDLY_OFFSET;
-	state->f.datalen = state->f.samples = samples;
-	/* create a buffer containing the digital milliwatt pattern */
-	for (i = 0;  i < samples;  i++)
-	{
-		state->buf[CW_FRIENDLY_OFFSET + i] = digital_milliwatt[state->index];
-		state->index = (state->index + 1) & 7;
-	}
-	return &state->f;
-}
-
-static struct cw_generator milliwattgen = 
-{
-	alloc: milliwatt_alloc,
-	release: milliwatt_release,
-	generate: milliwatt_generate,
-} ;
 
 static int milliwatt_exec(struct cw_channel *chan, int argc, char **argv, char *result, size_t result_max)
 {
+	static char deprecated = 0;
 	struct localuser *u;
+
+	if (!deprecated) {
+		deprecated = 1;
+		cw_log(CW_LOG_WARNING, "Milliwatt is deprecated. Use either Playtones(1004/0) or Playback(...) (to avoid transcoding)\n");
+	}
 
 	LOCAL_USER_ADD(u);
 
-	cw_set_write_format(chan, CW_FORMAT_ULAW);
-	cw_set_read_format(chan, CW_FORMAT_ULAW);
 	if (chan->_state != CW_STATE_UP)
-	{
 		cw_answer(chan);
-	}
-	if (cw_generator_activate(chan, &chan->generator, &milliwattgen, "milliwatt") < 0)
-	{
-		cw_log(CW_LOG_WARNING,"Failed to activate generator on '%s'\n",chan->name);
-		LOCAL_USER_REMOVE(u);
-		return -1;
-	}
-	while(!cw_safe_sleep(chan, 10000));
 
-	cw_generator_deactivate(&chan->generator);
+	if (!cw_playtones_start(chan, 0, "1004/0", 0))
+		while (!cw_safe_sleep(chan, 10000));
+
+	cw_playtones_stop(chan);
+
 	LOCAL_USER_REMOVE(u);
 	return -1;
 }
@@ -141,9 +87,6 @@ static int unload_module(void)
 
 static int load_module(void)
 {
-	if (!milliwattgen.is_initialized)
-		cw_object_init(&milliwattgen, &cw_object_isa_generator, CW_OBJECT_CURRENT_MODULE, 0);
-
 	milliwatt_app = cw_register_function(milliwatt_name, milliwatt_exec, milliwatt_synopsis, milliwatt_syntax, milliwatt_descrip);
 	return 0;
 }
